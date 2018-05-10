1cb5774229529c9a68b1906fab0c7fc7320e7789
rbishal50 <rbishal50@gmail.com>
2018-05-10 21:49:30 +0545

    initial commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+// import axios from 'axios';
+// import axios from '../../axios';
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from "../../hoc/asyncComponent";
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
+
+class Blog extends Component {
+  state = {
+    auth: true
+  };
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/posts/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline"
+                  }}
+                >
+                  Posts
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true"
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+          {this.state.auth ? (
+            <Route path="/new-post" component={AsyncNewPost} />
+          ) : null}
+          <Route path="/posts" component={Posts} />
+          {/*<Redirect from="/" to="/posts/" />*/}
+          <Route render={() => <h1>404 Not Found</h1>} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Blog;
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import axios from "../../../axios";
+import { Route } from "react-router-dom";
+
+import Post from "../../../components/Post/Post";
+import FullPost from "../FullPost/FullPost";
+import "./Posts.css";
+// import { Link } from 'react-router-dom';
+
+class Posts extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    console.log(this.props);
+    axios
+      .get("/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Bishal"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        // console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+        // this.setState({error: true});
+      });
+  }
+
+  postSelectedHandler = id => {
+    this.props.history.push({
+      pathname: "/posts/" + id
+    });
+    // this.props.history.push('/posts/' + id);
+  };
+
+  render() {
+    let posts = (
+      <p style={{ textAlign: "center" }}>Sorry, something went wrong</p>
+    );
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          // <Link
+          //     to={'/posts/' + post.id}
+          //     key={post.id}>
+          <Post
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+            key={post.id}
+          />
+          // </Link>
+        );
+      });
+    }
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <Route
+          path={this.props.match.url + "/:id"}
+          exact
+          component={FullPost}
+        />
+      </div>
+    );
+  }
+}
+
+export default Posts;
